9ff920106002e2dd6a71e7e1686333fe3808162e
anthonehi <78658590+anthonehi@users.noreply.github.com>
2021-06-11 17:30:19 -0700

    Update Startup.cs

--- SecretSanta/src/SecretSanta.Api/Startup.cs
+++ SecretSanta/src/SecretSanta.Api/Startup.cs
@@ -1,21 +1,32 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
 using SecretSanta.Business;
 using SecretSanta.Data;
-
+using System.Configuration;
+using Microsoft.Extensions.Configuration;
+using System;
+using Serilog;
+using Microsoft.Extensions.Logging;
 namespace SecretSanta.Api
 {
     public class Startup
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        private IConfigurationRoot Configuration;
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = (IConfigurationRoot)configuration ?? throw new ArgumentNullException($"{nameof(configuration)} was null");
+            System.Console.WriteLine();
+        }
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddScoped<IUserRepository, UserRepository>();
             services.AddScoped<IGroupRepository, GroupRepository>();
+            services.AddScoped<IGiftRepository, GiftRepository>();
             services.AddControllers();
             services.AddSwaggerDocument();
 
@@ -28,18 +39,28 @@ namespace SecretSanta.Api
                            .AllowAnyHeader();
                 });
             });
+            services.AddLogging(builder => {
+                builder.AddSerilog(logger: Log.Logger);
+            });
+            //services.Configure<SecretSanta.Data.DbContext>(options => Configuration.GetSection("Data").Bind(options));
+            services.AddDbContext<Data.DbContext>(options => options.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
 
-            /*services.AddDbContext<SecretSantaContext>(options =>
-                options.UseSqlite("Data Source=main.db"));*/
+            services.AddDbContext<SecretSanta.Data.DbContext>(options => 
+            {
+            options.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
+            options.UseLoggerFactory(LoggerFactory.Create( builder => 
+                        builder.AddSerilog(Log.Logger.ForContext<Data.DbContext>().ForContext("Category", "Database"))
+            ));
+            
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
+            
                 app.UseDeveloperExceptionPage();
-            }
+            
 
             app.UseOpenApi();
             app.UseSwaggerUi3();
